dfbebc5626868ddb74b85a0a5f1cf8f53a6d5753
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-24 15:51:46 +0530

    splice method

--- Splice.js
+++ Splice.js
@@ -1,14 +1,21 @@
-function Slice(array, a,b){
-  return array.reduce((result, current, i) => {
-    if (i >= a && i < b) {
-      result.push(current);
-    }           
-    return result;
-  }, []);
+function Splice(arr, start, deleteCount,...items) {
+    console.log(items.shift())
+return arr.reduce((result, current, index) => {
+   
+ if (index < start || index >= start + deleteCount) {
+   result.push(current);
+ } 
+else if (items.length > 0) {
+   result.push(items.shift());
+ }
+
+ return result;
+}, []);
+
 }
+const c=[1,2,3,5,6]
 
-const n = [1, 2, 3, 4, 5];
+const d= Splice(c,2,4,5,4,3,4)
+console.log(d)
 
 
-const s = Slice(n, 1, 4);
-console.log(s); 
