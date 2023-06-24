ef62705be2264f4026a74fa408f70f022d3925ae
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-24 15:48:18 +0530

    splice

---
+++ Splice.js
@@ -0,0 +1,14 @@
+function Slice(array, a,b){
+  return array.reduce((result, current, i) => {
+    if (i >= a && i < b) {
+      result.push(current);
+    }           
+    return result;
+  }, []);
+}
+
+const n = [1, 2, 3, 4, 5];
+
+
+const s = Slice(n, 1, 4);
+console.log(s); 
