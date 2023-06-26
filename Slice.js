a3e5c63b1f1ad26adbd21bfb69d903fa3c90df3f
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-26 10:52:43 +0530

    filter method

--- Slice.js
+++ Slice.js
@@ -12,3 +12,4 @@ const n = [1, 2, 3, 4, 5];
 
 const s = Slice(n, 1, 4);
 console.log(s); 
+
---
+++ filter.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filter = numbers.reduce((a, b) => {
+  if (b % 2 == 0) {
+    a.push(b);
+  }
+  return a;
+}, []);
+
+console.log(filter);
