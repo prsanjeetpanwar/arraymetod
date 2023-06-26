e49693d5cf0d6422d31fe3232d89d2038e2b6c7d
prsanjeet panwar <prsanjeetpanwar729@gmail.com>
2023-06-26 11:34:43 +0530

    updated splice

--- Splice.js
+++ Splice.js
@@ -1,21 +1,18 @@
-function Splice(arr, start, deleteCount,...items) {
-    console.log(items.shift())
-return arr.reduce((result, current, index) => {
-   
- if (index < start || index >= start + deleteCount) {
-   result.push(current);
- } 
-else if (items.length > 0) {
-   result.push(items.shift());
- }
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
 
- return result;
-}, []);
-
+function splice(arr, start, deleteCount, ...items) {
+  return arr.reduce((result, current, index) => {
+    if (index < start || index >= start + deleteCount) {
+      result.push(current);
+    } else if (items.length > 0) {
+      result = result.concat(items);
+      items = []; 
+    }
+    return result;
+  }, []);
 }
-const c=[1,2,3,5,6]
 
-const d= Splice(c,2,4,5,4,3,4)
-console.log(d)
-
-
+const c = [1, 2, 3, 5, 6];
+const d = splice(c, 2, 2, 4, 7, 8,4,5,2,6,246,2,25,26,123);
+console.log(d);
